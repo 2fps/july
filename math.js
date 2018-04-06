dab81ce86179800ff6c8b2d2b9b6c95091263906
2fps <echoweb@126.com>
2018-04-06 16:16:32 +0800

    add Math module

---
+++ math.js
@@ -0,0 +1,10 @@
+/**
+ * 求数组中的最大值
+ * @Author   zyt
+ * @DateTime 2018-04-06T16:13:32+0800
+ * @return   {Array}                 一系列数字组合
+ */
+function max() {
+    return Math.max.apply(null, arguments);
+}
+exports.max = max;
