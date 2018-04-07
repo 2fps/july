a5cb25aeae0e6302f8b08f541b1a3fbc1ed12c55
2fps <echoweb@126.com>
2018-04-07 09:29:01 +0800

    modify math.js

--- math.js
+++ math.js
@@ -4,7 +4,19 @@
  * @DateTime 2018-04-06T16:13:32+0800
  * @return   {Array}                 一系列数字组合
  */
-function max() {
+function max () {
     return Math.max.apply(null, arguments);
 }
+
+/**
+ * 求数组中的最小值
+ * @Author   zyt
+ * @DateTime 2018-04-06T21:43:08+0800
+ * @return   {Aray}                 一系列数字组合
+ */
+function min () {
+    return Math.min.apply(null, arguments);
+}
+
 exports.max = max;
+exports.min = min;
