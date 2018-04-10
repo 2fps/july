5fefd042e2689a421cd6d7a61f9cbbd0c9cc5500
2fps <echoweb@126.com>
2018-04-10 22:32:42 +0800

    modify array module

--- array.js
+++ array.js
@@ -7,10 +7,32 @@
  */
 function isArray (value) {
     if (Array.isArray) {
-        return Array.isArray(value); //IE9+
+        return Array.isArray(value); // IE9+
     } else {
         return Object.prototype.toString.call(vlaue) === "[object Array]";
     }
 }
 
+/**
+ * 数组内容匹配
+ * @Author   zyt
+ * @DateTime 2018-04-10T22:28:11+0800
+ * @param    {Array}                 arr  要遍历的数组
+ * @param    {[type]}                 val 要匹配的值
+ * @return   {Number}                     匹配到数组中的位置，-1表示没有匹配上
+ */
+function indexOf (arr, val) {
+    if (Array.indexOf) { // IE9+
+        return arr.indexOf(val);
+    } else {
+        for (var i = 0, len = arr.length; i < len; ++i) {
+            if (arr[i] === val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
 exports.isArray = isArray;
+exports.indexOf = indexOf;
