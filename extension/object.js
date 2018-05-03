491ad234e0f67d5985ad880a4635a221a29bcbcf
2fps <echoweb@126.com>
2018-05-03 23:07:10 +0800

    add module object

---
+++ extension/object.js
@@ -0,0 +1,15 @@
+create = (function () {
+    function F () {}
+
+    return function (o) {
+        if (arguments.length != 1) {
+            throw new Error('Object.create implementation only accepts one parameter.');
+        }
+
+        F.prototype = o;
+
+        return new F()
+    }
+})();
+
+exports.create = create;
