cb130f56fa1cbcf21acd08c6895a679285d232b3
2fps <echoweb@126.com>
2018-05-07 22:34:21 +0800

    add function isEmptyObject

--- extension/object.js
+++ extension/object.js
@@ -8,8 +8,27 @@ create = (function () {
 
         F.prototype = o;
 
-        return new F()
-    }
+        return new F();
+    };
 })();
 
+/**
+ * 检测对象是否是空对象，如{}
+ * @Author   zyt
+ * @DateTime 2018-05-07T22:32:51+0800
+ * @param    {Object}                 obj 要检测的对象
+ * @return   {Boolean}                    是否是空对象
+ */
+function isEmptyObject (obj) {
+    var name;
+
+    for (name in obj) {
+        return false;
+    }
+    return true;
+}
+
+
+
 exports.create = create;
+exports.isEmptyObject = isEmptyObject;
