a721eb4d199a82ee7489920b8acbbdd28f10c31b
2fps <echoweb@126.com>
2018-05-11 20:36:30 +0800

    modify regexp module

--- extension/regexp.js
+++ extension/regexp.js
@@ -20,5 +20,17 @@ function isIdCard (str) {
     return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(str);
 }
 
+/**
+ * 判断是否是手机号
+ * @Author   zyt
+ * @DateTime 2018-05-09T21:49:08+0800
+ * @param    {String}                 str 手机字符串
+ * @return   {Boolean}                    是否是手机号码
+ */
+function isPhoneNumber (str) {
+    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str);
+}
+
 exports.isEmail = isEmail;
 exports.isIdCard = isIdCard;
+exports.isPhoneNumber = isPhoneNumber;
