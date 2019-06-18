8e9575d1363780581a87e875187dfb98fb690939
2fps <echoweb@126.com>
2019-06-18 22:28:24 +0800

    整改

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const package = require('./package.json');
+
+let config = {
+    // 入口
+    entry: path.resolve(__dirname, 'src/index.ts'),
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './src'
+    },
+    output: {
+        // 输出文件名
+        filename: 'july.js',
+        // 输出路径
+        path: path.resolve(__dirname, 'dist'),
+        libraryExport: "default",
+        library: "July",
+        libraryTarget: "umd"
+    },
+    module: {
+        unknownContextCritical : false,
+        rules:[{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.json'],
+    },
+    plugins: [
+        // 文件注释插件
+        new webpack.BannerPlugin(`
+${ package.name } - ${ package.description }
+
+@version v${ package.version }
+@homepage ${ package.homepage }
+@author ${ package.author } <echoweb@126.com> (http://www.zhuyuntao.cn)
+@license ${ package.license }
+        `)
+    ],
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        // 开发模式下才要用到html
+        config.plugins.push(
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'src/example.html'),
+                filename: 'index.html'
+            }
+        ));
+    }
+    
+    return config;
+}
